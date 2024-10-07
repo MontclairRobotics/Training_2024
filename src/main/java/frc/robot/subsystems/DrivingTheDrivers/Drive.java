25e7953f1a7f2b3238f171d53998241d6578b1a6
Bry <wobry20@gmail.com>
2024-10-07 15:57:10 -0400

    Added motors to swerve thingy

--- src/main/java/frc/robot/subsystems/DrivingTheDrivers/Drive.java
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/Drive.java
@@ -2,18 +2,30 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.DrivingTheDrivers;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Drive extends SubsystemBase {
-
-  private PIDController pid;
+  
   /** Creates a new ExampleSubsystem. */
   public Drive() {
     pid = new PIDController(-1,-1,-1);
+    canSparkLeftForward = new CANSparkMax(1, MotorType.kBrushless);
+    canSparkRightForward = new CANSparkMax(2, MotorType.kBrushless);
+    canSparkLeftBack = new CANSparkMax(3, MotorType.kBrushless);
+    canSparkRightBack = new CANSparkMax(4, MotorType.kBrushless);
+    canBusName = "rio";
+    canCoder = new CANcoder(1, canBusName);
+    canSparkLeftForward.set(pid.calculate((canCoder.getDistance(), PlaceholderSetpoint));
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.DrivingTheDrivers;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+
+private CANSparkMax turn;
+private CANSparkMax Falconmotorthing;
+private PIDController pid;
+private String canBusName;
+private CANcoder canCoder;
+public class SwerveModule {
+    
+    //  PID controller for these motors
+    // cancoders
+}
