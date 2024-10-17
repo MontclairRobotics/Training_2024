4cb213bb2286402544fa8f96e06edb9a3c0a0015
cbgpenguin <cbaneygiampoala@gmail.com>
2024-10-17 12:12:07 -0400

    WIP More drive work
    Co-authored-by: Bry <BCODERW@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/DrivingTheDrivers/Drive.java
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/Drive.java
@@ -10,54 +10,31 @@ import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Drive extends SubsystemBase {
-  
-  /** Creates a new ExampleSubsystem. *
+  private SwerveModule forwardRight;
+  private SwerveModule forwardLeft;
+  private SwerveModule backRight;
+  private SwerveModule backLeft;
+  private Translation2d posForwardRight;
+  private Translation2d posForwardLeft;
+  private Translation2d posBackRight;
+  private Translation2d posBackLeft;
   public Drive() {
-
-    canSparkLeftForward = new CANSparkMax(1, MotorType.kBrushless);
-    canSparkRightForward = new CANSparkMax(2, MotorType.kBrushless);
-    canSparkLeftBack = new CANSparkMax(3, MotorType.kBrushless);
-    canSparkRightBack = new CANSparkMax(4, MotorType.kBrushless);
-    canBusName = "rio";
-    canCoder = new CANcoder(1, canBusName);
-  }
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    forwardRight = new SwerveModule(1,1,1,0.0,0.0);
+    forwardLeft = new SwerveModule(1,1,1,0.0,0.0);
+    backRight = new SwerveModule(1,1,1,0.0,0.0);
+    backLeft = new SwerveModule(1,1,1,0.0,0.0);
+    posForwardRight = forwardRight.getPos();
+    posForwardLeft = forwardLeft.getPos(); // bla bla unfinished 
+    posBackRight = backRight.getPos();
+    posBackLeft = backLeft.getPos();
+  // here we  need an object of  SwerveDriveKinematics made with a Translation2d that cad has values for or somthing smh
+        // SwerveModuleState[] swerveModuleStates =
+        //     kinematics.toSwerveModuleStates(velocity, centerOfRotationMeters); // prety sure  all this stuff I'm doing goes in a different part of the file but I'll do real stuff tomawrrow this is just stuff abe told me about so now I'm writing it
   }
 }
--- src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
@@ -7,6 +7,7 @@ import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
@@ -15,27 +16,31 @@ public class SwerveModule {
     private TalonFX falconMotorThing;
     private CANSparkMax turnMotor;
     private PIDController pid;
-    private String canBusName;
+    private String canBusName;  
     private CANcoder canCoder;
     private SwerveModuleState moduleState;
     private double tempXTarget = 10; //remove soon this represents the speeds were trying to get to and stuff (this is me writing code out probobly all wrong)
     private double tempYTarget = 10;
     private double tempRotationTarget = 90;
-    private Rotation2d tempRotation2d = 30.0; //flip its a type mismach I don't know how they store the scary radian unit cirle robot rotation numbers! they have like a multible and then sin and cos right? Why must they do this to us! .·°՞(≧□≦)՞°·.
+    private Rotation2d tempRotation2d = new Rotation2d(0,0);
+    private Translation2d position;
+
     
-    public SwerveModule(int id) { //Hey wouldn't you need to ids or wait three? at least if we do it this way right? maybe I'm worng
+    public SwerveModule(int id1, int id2, int id3, double x, double y) { //Hey wouldn't you need to ids or wait three? at least if we do it this way right? maybe I'm worng
 
-        canCoder = new CANcoder(id,"useless");
-        turnMotor = new CANSparkMax(id, MotorType.kBrushless);
-        falconMotorThing = new TalonFX(id);
+        canCoder = new CANcoder(id1,"useless");
+        turnMotor = new CANSparkMax(id2, MotorType.kBrushless);
+        falconMotorThing = new TalonFX(id3);
         pid = new PIDController(1,1,1);
-
+        position = new Translation2d(x,y);
         ChassisSpeeds.fromFieldRelativeSpeeds( //this "Converts a user provided field-relative set of speeds into a robot-relative ChassisSpeeds object."
             tempXTarget, tempYTarget, tempRotationTarget, /*getRobotHeading()*/ tempRotation2d); //aww we need that method at some point (returns us a rotation 2d of where facing I think)
-        // here we need an object of  SwerveDriveKinematics made with all of our Translation2d that cad has values for or somthing smh
-        // SwerveModuleState[] swerveModuleStates =
-        //     kinematics.toSwerveModuleStates(velocity, centerOfRotationMeters); // prety sure  all this stuff I'm doing goes in a different part of the file but I'll do real stuff tomawrrow this is just stuff abe told me about so now I'm writing it
                 moduleState = new SwerveModuleState(); 
         
     }
+
+    public Translation2d getPos() {
+        return position;
+    }
+    
 }
