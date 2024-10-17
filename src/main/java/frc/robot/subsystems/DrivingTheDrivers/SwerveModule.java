869c5687a13088e630151a333106526e6c8be91e
cbgpenguin <cbaneygiampoala@gmail.com>
2024-10-17 00:40:56 -0400

    WIP SwerveModule layout
    Does not work!!!! Just laying out what we probably might need to do

--- src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
@@ -2,41 +2,40 @@ package frc.robot.subsystems.DrivingTheDrivers;
 
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveModule {
 
     private TalonFX falconMotorThing;
+    private CANSparkMax turnMotor;
     private PIDController pid;
     private String canBusName;
     private CANcoder canCoder;
     private SwerveModuleState moduleState;
+    private double tempXTarget = 10; //remove soon this represents the speeds were trying to get to and stuff (this is me writing code out probobly all wrong)
+    private double tempYTarget = 10;
+    private double tempRotationTarget = 90;
+    private Rotation2d tempRotation2d = 30.0; //flip its a type mismach I don't know how they store the scary radian unit cirle robot rotation numbers! they have like a multible and then sin and cos right? Why must they do this to us! .·°՞(≧□≦)՞°·.
+
     
-    public SwerveModule(int id) {
+    public SwerveModule(int id) { //Hey wouldn't you need to ids or wait three? at least if we do it this way right? maybe I'm worng
+
         canCoder = new CANcoder(id,"useless");
-        turn = new CANSparkMax(id, MotorType.kBrushless);
+        turnMotor = new CANSparkMax(id, MotorType.kBrushless);
         falconMotorThing = new TalonFX(id);
         pid = new PIDController(1,1,1);
-    }
-  
-    
-    
 
-    public void drive(double speed){
-        // this.speed = speed;
-        // falconMotorThing.set(pid.calculate(canCoder.getAbsolutePosition(), speed));
+        ChassisSpeeds.fromFieldRelativeSpeeds( //this "Converts a user provided field-relative set of speeds into a robot-relative ChassisSpeeds object."
+            tempXTarget, tempYTarget, tempRotationTarget, /*getRobotHeading()*/ tempRotation2d); //aww we need that method at some point (returns us a rotation 2d of where facing I think)
+        // here we need an object of  SwerveDriveKinematics made with a Translation2d that cad has values for or somthing smh
+        // SwerveModuleState[] swerveModuleStates =
+        //     kinematics.toSwerveModuleStates(velocity, centerOfRotationMeters); // prety sure  all this stuff I'm doing goes in a different part of the file but I'll do real stuff tomawrrow this is just stuff abe told me about so now I'm writing it
+                moduleState = new SwerveModuleState(); 
     }
-
-    public void turn(double angle) {
-        // this.angle = angle;
-        // turn.set(pid.calculate(canCoder.getAbsolutePosition(), angle));
-    }
-    //  PID controller for these motors
-    // cancoders
-    
 }
-    
