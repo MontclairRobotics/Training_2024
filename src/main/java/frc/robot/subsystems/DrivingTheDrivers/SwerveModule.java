a7b23a6ce4e1f88ffc8ced7eff27dfe38c29668d
Bry <wobry20@gmail.com>
2024-10-23 13:37:21 -0400

    Some PID stuff with motors
    So these motors seem to dislike wpilib's PID, or at least like their own. We can still keep the setpoint with the swervestates.
    
    Im mostly sure this is all correct

--- src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
@@ -1,9 +1,13 @@
 package frc.robot.subsystems.DrivingTheDrivers;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -23,14 +27,34 @@ public class SwerveModule {
     private Translation2d position;
 
     
-    public SwerveModule(int id1, int id2, int id3, double x, double y) { //Hey wouldn't you need to ids or wait three? at least if we do it this way right? maybe I'm worng
-
+    public SwerveModule(int id1, int id2, int id3, double x, double y) { 
+        // We also need to put the data (swervestates) into here so we know the setpoint/request with velocity
         canCoder = new CANcoder(id1,"useless");
         turnMotor = new CANSparkMax(id2, MotorType.kBrushless);
         falconMotorThing = new TalonFX(id3);
         pid = new PIDController(1,1,1);
         position = new Translation2d(x,y);
         moduleState = new SwerveModuleState(); 
+        
+        // we need revpid controls :)!!!
+        SparkPIDController m_pidController = turnMotor.getPIDController();
+        m_pidController.setP(0.5);
+        m_pidController.setI(0.5);
+        m_pidController.setD(0.5);
+        m_pidController.setIZone(0.5);
+        // we need to tune :(
+        m_pidController.setReference(1, CANSparkBase.ControlType.kVelocity);
+
+        var slot0Configs = new Slot0Configs();
+        slot0Configs.kS = 0.1; // Add 0.1 V output to overcome static friction
+        slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        slot0Configs.kP = 0.11; // An error of 1 rps results in 0.11 V output
+        slot0Configs.kI = 0; // no output for integrated error
+        slot0Configs.kD = 0; // no output for error derivative      
+        falconMotorThing.getConfigurator().apply(slot0Configs); //configures stuff idk exactly
+        final VelocityVoltage m_request = new VelocityVoltage(0).withSlot(0);
+        falconMotorThing.setControl(m_request.withVelocity(8).withFeedForward(0.5));
+        // a lot of values will get changed, and I hope this is right
         
     }
 
