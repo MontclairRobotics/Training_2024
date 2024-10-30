aa612c04f5b031125ceb417097a007e2be578894
cbgpenguin <cbaneygiampoala@gmail.com>
2024-10-30 00:32:03 -0400

    more descriptive variable names + comments
    It's 12:30 and I'm board.

--- src/main/java/frc/robot/subsystems/DrivingTheDrivers/Drive.java
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/Drive.java
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.DrivingTheDrivers;
-import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax; // Pretty sure we don't need this motor stuff here because its handled in the swerve module class? No?
 import com.revrobotics.SparkPIDController;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkBase;
@@ -29,8 +29,8 @@ public class Drive extends SubsystemBase {
   private Translation2d posForwardLeft;
   private Translation2d posBackRight;
   private Translation2d posBackLeft;
-  private SwerveDriveKinematics roboSwerveKinematics; // we should probably change the name later
-  private SwerveModuleState[] swerveModuleStates;
+  private SwerveDriveKinematics roboSwerveKinematics; // TODO: we should probably change the name later
+  private SwerveModuleState[] swerveModuleStates; //TODO: figure out why this says not being used all wierd.
   private double tempXTarget = 10; //remove soon this represents the speeds were trying to get to and stuff (this is me writing code out probobly all wrong)
   private double tempYTarget = 10;
   private double tempRotationTarget = 90;
@@ -38,13 +38,13 @@ public class Drive extends SubsystemBase {
 
   public Drive() {
     forwardRight = new SwerveModule(1,1,1,Constants.SwerveModuleConstants.forwardRightSwerveX,Constants.SwerveModuleConstants.forwardRightSwerveY); //these x and y constants are all 0.0 for now, and found under constants
-    forwardLeft = new SwerveModule(1,1,1,Constants.SwerveModuleConstants.forwardLeftSwerveX,Constants.SwerveModuleConstants.forwardLeftSwerveY);
+    forwardLeft = new SwerveModule(1,1,1,Constants.SwerveModuleConstants.forwardLeftSwerveX,Constants.SwerveModuleConstants.forwardLeftSwerveY); //if CAD won't tell us we can tottally just look in the old robot code of this. 
     backRight = new SwerveModule(1,1,1,Constants.SwerveModuleConstants.backRightSwerveX,Constants.SwerveModuleConstants.backRightSwerveY);
     backLeft = new SwerveModule(1,1,1,Constants.SwerveModuleConstants.backLeftSwerveX,Constants.SwerveModuleConstants.backLeftSwerveY);
     roboSwerveKinematics = new SwerveDriveKinematics(posForwardLeft,posForwardRight,posBackLeft,posBackRight);
     swerveModuleStates = roboSwerveKinematics.toSwerveModuleStates(ChassisSpeeds.fromFieldRelativeSpeeds(
-    tempXTarget, tempYTarget, tempRotationTarget, tempRotation2d));
-    // next we need to do the PID stuff. I'm mostly sure the Swerve module states are what we put into PID. For the forwardLeft one, we input forwardLeft's current state, and the swerve states with an index of 0. Then forward right for current and states index 1 would be for changing forward right, etc. This is how wpilib has it: motor.set(pid.calculate(encoder.getDistance(), setpoint));
+    tempXTarget, tempYTarget, tempRotationTarget, tempRotation2d)); //TODO: fix this
+    // next we need to do the PID stuff.The Swerve module states are what we put into PID (AND if tuned right should output volteges to feedinto out motors). For the forwardLeft one, we input forwardLeft's current state, and the swerve states with an index of 0. Then forward right for current and states index 1 would be for changing forward right, etc. This is how wpilib has it: motor.set(pid.calculate(encoder.getDistance(), setpoint));
     // for PID we may have to do different stuff just because the motors may require their own methods. PID also has a setpoint method
 
 
--- src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.DrivingTheDrivers;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkBase;
@@ -10,46 +8,44 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
+//import edu.wpi.first.math.geometry.Rotation2d; ////were not using this here are we?
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
+//import edu.wpi.first.math.kinematics.ChassisSpeeds; //were not using this here are we?
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveModule {
 
-    private TalonFX falconMotorThing;
-    private CANSparkMax turnMotor;
+    private TalonFX falconMotorDrive;
+    private CANSparkMax canTurnMotor;
     private PIDController pid;
-    private String canBusName;  
-    private CANcoder canCoder;
+    private CANcoder canCoder; //wholy cow my java must be gone because why is it say that these go unused???? I'm probobly blind
     private SwerveModuleState moduleState;
-
     private Translation2d position;
 
     
-    public SwerveModule(int id1, int id2, int id3, double x, double y) { 
+    public SwerveModule(int canCoderID, int canTurnMotorID, int falconMotorDriveID, double xTranslation2d, double yTranslation2d) { 
         // We also need to put the data (swervestates) into here so we know the setpoint/request with velocity
-        canCoder = new CANcoder(id1,"rio");
-        turnMotor = new CANSparkMax(id2, MotorType.kBrushless);
-        falconMotorThing = new TalonFX(id3);
+        canCoder = new CANcoder(canCoderID,"rio"); //changed name to rio here (must be called rio can't just make up a name)
+        canTurnMotor = new CANSparkMax(canTurnMotorID, MotorType.kBrushless);
+        falconMotorDrive = new TalonFX(falconMotorDriveID);
         pid = new PIDController(1,1,1);
-        position = new Translation2d(x,y);
+        position = new Translation2d(xTranslation2d,yTranslation2d);
         moduleState = new SwerveModuleState(); 
-        // we need revpid controls :)!!!
-        SparkPIDController m_pidController = turnMotor.getPIDController();
-        m_pidController.setP(0.5);
-        m_pidController.setI(0.5);
+        // TODO: we need revpid controls :)!!!
+        // what does the above comment mean? I have no clue
+        SparkPIDController m_pidController = canTurnMotor.getPIDController(); //hmm just noticed that this says to do cansparkbase.getpidcontroller to make a pid controller but I don't know.
+        m_pidController.setP(0.5); //  also not sure what the M_PID is? maybe can explain to me? (not that I looked)
+        m_pidController.setI(0.5); //I gota review documentation about pid to Find out how to put the swerve modual state in and get the voltage out and also figure out how to use sisID or whaterver its called.
         m_pidController.setD(0.5);
         m_pidController.setIZone(0.5);
-        // we need to tune w/ voltage to get an output that is in voltages so we can put it in the move thing
+        // TODO: we need to tune w/ voltage to get an output that is in voltages so we can put it in the move thing
         m_pidController.setReference(1, CANSparkBase.ControlType.kVelocity);
-        // a lot of values will get changed, and I hope this is right, and we also if we wanted we can put some of these in methods to clean up the code but we don't need to
-        
+        //TODO: gota fix this mess later         
     }
 
     public void move(double falconVoltage, double turnVoltage) {  // this should acutally work the one thing we have to do now is get the modual state into the the PID. LET's Do this!
-        falconMotorThing.setVoltage(falconVoltage);
-        turnMotor.set(turnVoltage);
+        falconMotorDrive.setVoltage(falconVoltage);
+        canTurnMotor.set(turnVoltage);
     }
    
 }
