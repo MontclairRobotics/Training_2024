26499a2a75cfe2380da906ede7fa42e4155f96d4
Montclair Code Team <869c568+cbgpenguin@users.noreply.github.com>
2024-11-25 15:54:04 -0500

    blurg

--- src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/DrivingTheDrivers/SwerveModule.java
@@ -14,20 +14,23 @@ public class SwerveModule {
     private CANSparkMax canTurnMotor;
     private PIDController RotationPID;
     private PIDController DrivePID;
-    private CANcoder canCoder;
+    private CANcoder canSparkCoder;
     private double driveVoltage;
     private double turnVoltage;
     private SwerveModuleState state;
+    private CANcoder falconMotorEncoder;
     
 
     
     public SwerveModule(int canCoderID, int canTurnMotorID, int falconMotorDriveID) {
         
+        
+        canSparkCoder = new CANcoder(canCoderID,"rio"); //canbus must be named "rio"
+        falconMotorEncoder = new CANcoder(falconMotorDriveID, "rio");
 
-        canCoder = new CANcoder(canCoderID,"rio"); //canbus must be named "rio"
         canTurnMotor = new CANSparkMax(canTurnMotorID, MotorType.kBrushless);
         falconMotorDrive = new TalonFX(falconMotorDriveID);
-
+        
         RotationPID = new PIDController(0.5,0,0); //placeholder values for PID
         DrivePID  = new PIDController(0.5,0,0); //placeholder values for PID
     }
@@ -35,8 +38,8 @@ public class SwerveModule {
     public void move() { //I think abe said that some varriables in here dont need to be fore the class but just for here better
         
         
-        driveVoltage = DrivePID.calculate(findCurrentSpeedHerePlease, state.speedMetersPerSecond); //TODO: currentDriveVoltage needs to be replaced with current speed cant mismatch units opps
-        turnVoltage = RotationPID.calculate(canCoder.getPosition().getValue()/360, state.angle.getDegrees());/*canCoder.getPosition().getValue() is in rotations not deggres so i devided by 360 although we proboby want to change all our units later*/ // This one needed .getDegrees() because swervemodulestates stores a rotation 2d no degrees but I did some snooping in the class and found this.
+        driveVoltage = DrivePID.calculate(falconMotorEncoder.getVelocity(), state.speedMetersPerSecond); //TODO: currentDriveVoltage needs to be replaced with current speed cant mismatch units opps
+        turnVoltage = RotationPID.calculate(canSparkCoder.getPosition().getValue()/360, state.angle.getDegrees());/*canSparkCoder.getPosition().getValue() is in rotations not deggres so i devided by 360 although we proboby want to change all our units later*/ // This one needed .getDegrees() because swervemodulestates stores a rotation 2d no degrees but I did some snooping in the class and found this.
         // TODO: Tune PID to get an output that is in voltages so we can put it in the move thing
 
         falconMotorDrive.setVoltage(driveVoltage);
